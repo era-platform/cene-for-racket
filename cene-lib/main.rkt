36b8627e60740d81dfbe12dd67e8537b29ecdba6
Ross Angle <rokitna@hotmail.com>
2018-06-29 02:14:34 -0700

    Write an implementation of Cene structs in the context of constructing and deconstructing them in Racket code.

--- cene-lib/main.rkt
+++ cene-lib/main.rkt
@@ -19,6 +19,75 @@
 ;   language governing permissions and limitations under the License.
 
 
+(require #/only-in racket/contract/base -> or/c)
+(require #/only-in racket/contract/region define/contract)
+
+(require #/only-in lathe-comforts dissect expect w-loop)
+(require #/only-in lathe-comforts/maybe just maybe/c nothing)
 (require #/only-in lathe-comforts/struct struct-easy)
 
-; TODO: Write this file.
+
+; TODO: Implement the Cene programming language.
+
+
+; NOTE: The abbreviation "cv" stands for "Cene value." (TODO: At some
+; point when we have a Racket accessible from Cene through an FFI, we
+; may use "rv," standing for "Racket values," for Cene values that
+; wrap arbitrary first-class Racket values.)
+
+; NOTE: Although it is not very strictly enforced, there is an
+; intended format to the data here: The value of `tags` should be a
+; nonempty list of Effection name values, beginning with the main tag
+; name of the struct and then listing the names of the projections.
+; The projections' names should have no duplicates. The value of
+; `projs` should be a list of Cene values which are the values of the
+; projections.
+(struct-easy (cv-struct tags projs))
+
+(define/contract (make-cv-struct tags projs)
+  (-> pair? (or/c (list) pair?) cv-struct?)
+  ; NOTE: For efficiency, we don't do any checking here. The value of
+  ; `tags` should be a nonempty list of Effection name values,
+  ; beginning with the main tag name of the struct and then listing
+  ; the names of the projections. The projections' names should have
+  ; no duplicates. The value of `projs` should be a list of Cene
+  ; values which are the values of the projections.
+  (cv-struct tags projs))
+
+(define/contract (unmake-cv-struct-maybe tags s)
+  (-> pair? cv-struct? #/maybe/c #/or/c (list) pair?)
+  (dissect s (cv-struct s-tags s-projs)
+  
+  ; NOTE: This is the happy path. Our struct representation is the way
+  ; it is so that we can usually use an object identity comparison
+  ; like this instead of traversing the projections.
+  #/if (eq? tags s-tags) (just s-projs)
+  
+  #/dissect tags (cons main-tag proj-tags)
+  #/dissect s-tags (cons s-main-tag s-proj-tags)
+  ; TODO: See if `equal?` is the proper way to compare Effection name
+  ; values.
+  #/expect (equal? main-tag s-main-tag) #t (nothing)
+  #/expect
+    (w-loop next proj-tags proj-tags s-projs s-projs proj-hash (hash)
+      (expect proj-tags (cons proj-tag proj-tags)
+        (expect s-projs (list)
+          (error "Encountered a cv-struct with more projection values than projection tags")
+        #/just proj-hash)
+      #/expect s-projs (list s-proj s-projs)
+        (error "Encountered a cv-struct with more projection tags than projection values")
+      #/if (hash-has-key? proj-hash proj-tag) (nothing)
+      #/next proj-tags s-projs #/hash-set proj-hash proj-tag s-proj))
+    (just proj-hash)
+    (nothing)
+  #/w-loop next
+    proj-hash proj-hash
+    proj-tags proj-tags
+    rev-projs (list)
+    
+    (expect proj-tags (cons proj-tag proj-tags)
+      (expect (hash-empty? proj-hash) #t (nothing)
+      #/just #/reverse rev-projs)
+    #/if (not #/hash-has-key? proj-hash proj-tag) (nothing)
+    #/next (hash-remove proj-hash proj-tag) proj-tags
+      (cons (hash-ref proj-hash proj-tag) rev-projs))))
