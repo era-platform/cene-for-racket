6b8e4be5110dca814f047f28351d295a13d9acae
Ross Angle <rokitna@hotmail.com>
2020-09-02 22:48:39 -0700

    In the EBNF syntax's tokenizer, reduce duplication by factoring out abbreviations for certain character sets.

--- cene-lib/private/parser.rkt
+++ cene-lib/private/parser.rkt
@@ -22,12 +22,48 @@
 (require #/only-in racket/pretty pretty-print)
 
 (require #/only-in brag/support token)
-(require #/only-in br-parser-tools/lex char-set lexeme lexer-src-pos)
+(require #/only-in br-parser-tools/lex
+  char-set define-lex-abbrev lexeme lexer-src-pos)
 (require #/only-in br-parser-tools/lex-sre + ~ ? * or seq)
 
 (require #/only-in lathe-comforts dissect fn mat)
 
 (require #/only-in cene/private/parser-grammar parse)
+
+
+(define-lex-abbrev lex-inline-whitespace (char-set " \t"))
+(define-lex-abbrev lex-cr "\r")
+(define-lex-abbrev lex-lf "\n")
+(define-lex-abbrev lex-whitespace
+  (or lex-inline-whitespace lex-cr lex-lf))
+
+(define-lex-abbrev lex-eq "=")
+(define-lex-abbrev lex-colon ":")
+(define-lex-abbrev lex-open-misc-bracket (char-set "([{"))
+(define-lex-abbrev lex-close-misc-bracket (char-set ")]}"))
+(define-lex-abbrev lex-backslash "\\")
+(define-lex-abbrev lex-slash "/")
+(define-lex-abbrev lex-open-angular-bracket "<")
+(define-lex-abbrev lex-neutral-angular-bracket "^")
+(define-lex-abbrev lex-close-angular-bracket ">")
+(define-lex-abbrev lex-dot ".")
+(define-lex-abbrev lex-pipe "|")
+(define-lex-abbrev lex-hash "#")
+(define-lex-abbrev lex-apropos-punctuation-mark
+  (or
+    lex-eq
+    lex-colon
+    lex-open-misc-bracket
+    lex-close-misc-bracket
+    lex-backslash
+    lex-slash
+    lex-open-angular-bracket
+    lex-neutral-angular-bracket
+    lex-close-angular-bracket
+    lex-dot
+    lex-pipe
+    lex-hash))
+    
 
 
 (define (beginningless-cene-lexer)
@@ -47,10 +83,10 @@
     ; and control characters in this. We should not include carriage
     ; returns or newlines.
     ;
-    [(+ #/char-set " \t") (token 'INLINE-WHITESPACE lexeme)]
+    [(+ lex-inline-whitespace) (token 'INLINE-WHITESPACE lexeme)]
     
     ; This matches carriage return, newline, or both in succession.
-    [(or (seq "\r" #/? "\n") "\n") (token 'NEWLINE lexeme)]
+    [(or (seq lex-cr #/? lex-lf) lex-lf) (token 'NEWLINE lexeme)]
     
     ; This matches any nonempty text that does not contain any of the
     ; various whitespace characters and punctuation marks we care
@@ -60,7 +96,7 @@
     ; are neither whitespace nor Pattern_Syntax.
     ;
     [
-      (+ #/~ #/char-set " \t\r\n=:()[]{}\\/<^>.|#")
+      (+ #/~ #/or lex-whitespace lex-apropos-punctuation-mark)
       (token 'IDENTIFIER lexeme)]
     
     ; This matches any colon-delimited text that does not contain
@@ -72,7 +108,7 @@
     ; known-good combination of scripts.
     ;
     [
-      (seq ":" (* #/~ #/char-set " \t\r\n:") ":")
+      (seq lex-colon (* #/~ #/or lex-whitespace lex-colon) lex-colon)
       (token 'GRAWLIX lexeme)]
     
     ; This matches "=" followed by any of the various punctuation
@@ -81,29 +117,31 @@
     ; TODO: Include non-ASCII Unicode Pattern_Syntax characters here.
     ;
     [
-      (seq "=" #/char-set "=:()[]{}\\/<^>.|#")
+      (seq lex-eq lex-apropos-punctuation-mark)
       (token 'ESCAPED-PUNCTUATION-MARK lexeme)]
     
     ; This matches an open bracket.
     ;
     ; TODO: Include other Unicode open brackets.
     ;
-    [(char-set "([{") (token 'OPEN-MISC-BRACKET lexeme)]
+    [lex-open-misc-bracket (token 'OPEN-MISC-BRACKET lexeme)]
     
     ; This matches a close bracket.
     ;
     ; TODO: Include other Unicode close brackets.
     ;
-    [(char-set ")]}") (token 'CLOSE-MISC-BRACKET lexeme)]
+    [lex-close-misc-bracket (token 'CLOSE-MISC-BRACKET lexeme)]
     
-    ["\\" (token 'BACKSLASH lexeme)]
-    ["/" (token 'SLASH lexeme)]
-    ["<" (token 'OPEN-ANGULAR-BRACKET lexeme)]
-    ["^" (token 'NEUTRAL-ANGULAR-BRACKET lexeme)]
-    [">" (token 'CLOSE-ANGULAR-BRACKET lexeme)]
-    ["." (token 'DOT lexeme)]
-    ["|" (token 'PIPE lexeme)]
-    ["#" (token 'HASH lexeme)]))
+    [lex-backslash (token 'BACKSLASH lexeme)]
+    [lex-slash (token 'SLASH lexeme)]
+    [lex-open-angular-bracket (token 'OPEN-ANGULAR-BRACKET lexeme)]
+    [
+      lex-neutral-angular-bracket
+      (token 'NEUTRAL-ANGULAR-BRACKET lexeme)]
+    [lex-close-angular-bracket (token 'CLOSE-ANGULAR-BRACKET lexeme)]
+    [lex-dot (token 'DOT lexeme)]
+    [lex-pipe (token 'PIPE lexeme)]
+    [lex-hash (token 'HASH lexeme)]))
 
 (define (cene-lexer)
   (define beginningless-lexer (beginningless-cene-lexer))
